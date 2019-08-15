41ce1240d3e1585ce36b27807863b271553f1b95
nootyface <cfstewart4@gmail.com>
2019-08-15 21:50:07 +0100

    Added initial scraping tutorial

---
+++ scraping.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+ 
+url_to_parse = "https://en.wikipedia.org/wiki/Python_(programming_language)"
+response = requests.get(url_to_parse)
+print(response)
+response_text = response.text
+soup = BeautifulSoup(response_text, 'lxml')
+all_main_headings = []
+main_headings = soup.find_all("li", class_="toclevel-1")
+for h1 in main_headings:
+    heading = h1.find("span", class_="toctext").text
+    all_main_headings.append(heading)
+    print(heading)
