1de9a5c1a87d9039a47068569459b9deba7041f6
nootyface <cfstewart4@gmail.com>
2019-08-17 19:29:44 +0100

    testing with local html file

--- scraping.py
+++ scraping.py
@@ -1,14 +1,14 @@
-import requests
 from bs4 import BeautifulSoup
- 
-url_to_parse = "https://en.wikipedia.org/wiki/Python_(programming_language)"
-response = requests.get(url_to_parse)
-print(response)
-response_text = response.text
-soup = BeautifulSoup(response_text, 'lxml')
-all_main_headings = []
-main_headings = soup.find_all("li", class_="toclevel-1")
-for h1 in main_headings:
-    heading = h1.find("span", class_="toctext").text
-    all_main_headings.append(heading)
-    print(heading)
+import requests
+
+with open('simple.html') as html_file:
+    soup = BeautifulSoup(html_file, 'lxml')
+    
+for article in soup.find_all('div', class_='article'):
+    headline = article.h2.a.text
+    print(headline)
+
+    summary = article.p.text
+    print(summary)
+
+    print()
